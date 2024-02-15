3265c98e8add0e79cef81ba7c752cd49865810c2
sebas313313313 <potosliping8@gmail.com>
2024-02-15 11:23:06 -0500

    controlador crear encuesta

--- backend/routers/respuestasRouter.js
+++ backend/routers/respuestasRouter.js
@@ -1,15 +1,19 @@
-import Express  from "express";
-import {
-    obtenerEncuestas
-} from '../controllers/respuesta.js'
-import checkAuth from '../middleware/checkAuth.js'
+import express from "express";
+import { obtenerEncuestas, agregarRespuesta, editarRespuesta, eliminarRespuesta } from '../controllers/respuestasController.js';
+import checkAuth from '../middleware/checkAuth.js';
 
-const router = Express.Router()
-router.route('/')
-    
-    .post(checkAuth,obtenerEncuestas)
-   
-    
+const router = express.Router();
 
+// Obtener todas las respuestas
+router.get('/', obtenerEncuestas);
 
-export default router
+// Agregar una nueva respuesta
+router.post('/', checkAuth, agregarRespuesta);
+
+// Editar una respuesta existente
+router.put('/:id', checkAuth, editarRespuesta);
+
+// Eliminar una respuesta existente
+router.delete('/:id', checkAuth, eliminarRespuesta);
+
+export default router;
