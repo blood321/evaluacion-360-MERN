2ebe6ddabb529de02fd72f1749b2b70468be5a98
Eider-Polar <114772645+Eider-Polar@users.noreply.github.com>
2024-02-24 19:58:09 -0500

    actualización de la ruta protegida
    lo nuevo que tiene es dependiendo de la ruta cambia el menu

--- frontend/src/layouts/RutaProtegida.jsx
+++ frontend/src/layouts/RutaProtegida.jsx
@@ -1,25 +1,44 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation} from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
-import Tematicas from "../components/Tematicas";
+import Menuprincipal from "../components/manuPrincipal";
+import MenuEvaluacion360 from "../components/menuEvaluacion360";
+import MenuCreacionPreguntas from "../components/menuPreguntas";
 
 function RutaProtegida() {
+  
+  const location = useLocation();
+  console.log(location)
   const { auth, cargando } = useAuth();
   if (cargando) return "cargando...";
+  
   return (
     <>
       {auth._id ? (
         <div className="bg-gray-100">
-          <Header />
-          <div className="md:flex md:min-h-screen">
-            <Sidebar />
-              <div className="items-center">
-                <Tematicas/>
-                <main className="py-5 pr-10 flex-1">
-                  <Outlet />
-              </main>
-            </div>
+          <div className="md:flex md:max-h-screen">
+            
+            {location.pathname === '/inicio' ? <Menuprincipal /> : null}
+            {location.pathname === '/inicio/evaluacion360' ?<MenuEvaluacion360 />  :null}
+
+            {location.pathname === '/inicio/crear-encuestas-aprendices' ?<MenuEvaluacion360 />  :null}
+            {location.pathname === '/inicio/crear-encuestas-autoevaluacion' ?<MenuEvaluacion360 />  :null}
+            {location.pathname === '/inicio/crear-encuestas-companeros' ?<MenuEvaluacion360 />  :null}
+            {location.pathname === '/inicio/crear-encuestas-jefes' ?<MenuEvaluacion360 />  :null}
+
+            {location.pathname === '/inicio/listar-encuestas' ?<Menuprincipal />  :null}
+
+            {location.pathname === '/inicio/crear-preguntas' ?<MenuCreacionPreguntas />  :null}
+            {location.pathname === '/inicio/crear-preguntas-aprendices' ?<MenuCreacionPreguntas />  :null}
+            {location.pathname === '/inicio/crear-preguntas-autoevaluacion' ?<MenuCreacionPreguntas />  :null}
+            {location.pathname === '/inicio/crear-preguntas-companeros' ?<MenuCreacionPreguntas />  :null}
+            {location.pathname === '/inicio/crear-preguntas-jefes' ?<MenuCreacionPreguntas />  :null}
+
+            <main className="py-5 pr-10 flex-1">
+              <Outlet />
+              
+            </main>
           </div>
         </div>
       ) : (
