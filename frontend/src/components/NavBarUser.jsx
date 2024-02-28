e1d03e825a487fcd44a8f89963c91de277a96ed1
JUAN CASTILLO <juansebastianq47@gmail.com>
2024-02-28 11:50:06 -0500

    update & create page frontend

--- frontend/src/components/NavBarUser.jsx
+++ frontend/src/components/NavBarUser.jsx
@@ -1,9 +1,20 @@
+import React, { useState } from 'react'
 import burgerMenu from '../assets/img/icons8-menú.svg'
 
 export const NavBarUser = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuOpen(prevState => !prevState)
+    }
+
     return (
         <>
-            <ul className=" hidden sm:flex text-[18px] sm:w-[438px] sm:place-content-around sm:text-[18px] sm:items-center md:text-Principal_1 md:font-semibold">
+            <ul
+                className={`hidden sm:flex text-[18px] sm:w-[438px] sm:place-content-around sm:text-[18px] sm:items-center text-Principal_1 md:font-semibold ${
+                    menuOpen ? '' : 'hidden'
+                }`}
+            >
                 <li>
                     <h2>Usuario</h2>
                 </li>
@@ -14,8 +25,14 @@ export const NavBarUser = () => {
                     <h2>Ficha</h2>
                 </li>
             </ul>
-            <img className="w-10 h-10 bg-cover cursor-pointer sm:hidden" src={burgerMenu} alt="Burger Menu" />
+            <img
+                className="w-10 h-10 bg-cover cursor-pointer sm:hidden"
+                src={burgerMenu}
+                alt="Burger Menu"
+                onClick={toggleMenu}
+            />
         </>
     )
 }
-export default NavBarUser;
+
+export default NavBarUser
