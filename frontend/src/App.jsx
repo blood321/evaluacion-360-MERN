0969ea35d0b161b0132c26ccc21c2fdfa22c1623
sebas313313313 <potosliping8@gmail.com>
2024-02-28 10:54:15 -0500

    Algunos cambios al front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,54 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Authlayout from './layouts/Authlayout'
-import RutaProtegida from './layouts/RutaProtegida'
-import CoorLayout from './layouts/CoorLayout'
-import LayoutSe from './layouts/LayoutSe.jsx'
-import Login from './paginas/Login'
-import Registrar from './paginas/Registrar'
-import OlvidePassword from './paginas/OlvidePassword'
-import NuevoPassword from './paginas/NuevoPassword'
-import ConfirmarCuenta from './paginas/ConfirmarCuenta'
-import Proyectos from './paginas/Proyectos'
-import NuevoProyecto from './paginas/NuevoProyecto'
-import Proyecto from './paginas/Proyecto'
-import EditarProyecto from './paginas/EditarProyecto'
-import Aloguin from './paginas/Aloguin'
-import Aviso from './paginas/Aviso'
-import Respoder from './paginas/Respoder'
-import { AuthProvider } from './context/AuthProvider'
-import { ProyectosProvider } from './context/ProyectosProvider'
-import LoginAdmin from './paginas/LoginAdmin.jsx'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Authlayout from "./layouts/Authlayout";
+import RutaProtegida from "./layouts/RutaProtegida";
+import CoorLayout from "./layouts/CoorLayout";
+import Login from "./paginas/Login";
+import Registrar from "./paginas/Registrar";
+import OlvidePassword from "./paginas/OlvidePassword";
+import NuevoPassword from "./paginas/NuevoPassword";
+import IniAdmin from "./paginas/IniAdmin.jsx";
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
+import Proyectos from "./paginas/Proyectos";
+import NuevoProyecto from "./paginas/NuevoProyecto";
+import Proyecto from "./paginas/Proyecto";
+import EditarProyecto from "./paginas/EditarProyecto";
+import Aloguin from "./paginas/Aloguin";
+import Aviso from "./paginas/Aviso";
+import { AuthProvider } from "./context/AuthProvider";
+import { ProyectosProvider } from "./context/ProyectosProvider";
+import LoginAdmin from "./paginas/LoginAdmin.jsx";
+import Componer from "./paginas/PrvComponents.jsx";
+import LayoutSe from "./layouts/LayoutSe.jsx";
 
 function App() {
- 
-    return (
-        <BrowserRouter>
-            <AuthProvider>
-                <ProyectosProvider>
-                    <Routes>
-                        <Route path="/" element={<Authlayout />}>
-                            <Route index element={<Aloguin />} />
-                        </Route>
-                       
-                        <Route path="/" element={<LayoutSe />}>
-                            <Route path="login-adm" element={<LoginAdmin />} />
-                        </Route> 
-                        
-                        <Route path="aviso/:email" element={<Aviso />} />
-                        <Route path="/Responder" element={<Respoder />} />
-                        <Route path="/" element={<CoorLayout />}>
-                            <Route path="login-coordinador" element={<Login />} />
-                            <Route path="registrar" element={<Registrar />} />
-                            <Route path="olvide-password" element={<OlvidePassword />} />
-                            <Route path="olvide-password/:token" element={<NuevoPassword />} />
-                            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <ProyectosProvider>
+          <Routes>
+            <Route path="/" element={<Authlayout />}>
+              <Route index element={<Aloguin />} />
+            </Route>
 
-                        </Route>
+            <Route path="/" element={<LayoutSe />}>
+              <Route path="InicioAdmin" element={<IniAdmin />} />
+            </Route>
+
+            
+            <Route path="Componer" element={<Componer />} />
+            <Route path="/aviso" element={<Aviso />} />
+              <Route path="/Login" element={<LoginAdmin />} />
+
+
+            <Route path="/" element={<CoorLayout />}>
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route
+                path="olvide-password/:token"
+                element={<NuevoPassword />}
+              />
+              <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+            </Route>
 
                         <Route path="proyectos" element={<RutaProtegida />}>
                             <Route index element={<Proyectos />} />
--- frontend/src/paginas/OlvidePassword.jsx
+++ frontend/src/paginas/OlvidePassword.jsx
@@ -2,47 +2,50 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import Alerta from "../components/Alerta";
 
-
 import clienteAxios from "../config/clienteAxios";
 
-
 const OlvidePassword = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [alerta, setAlerta] = useState({});
 
-  const handleSubmit = async e => {
-    e.preventDefault()
-    if (email === '' || email.length < 6) {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (email === "" || email.length < 6) {
       setAlerta({
-        msg: 'El email es obligatorio',
-        error: true
-      })
-      return
+        msg: "El email es obligatorio",
+        error: true,
+      });
+      return;
     }
     try {
-      const {data} = await clienteAxios.post("/usuarios/olvide-password", { email })
+      const { data } = await clienteAxios.post("/usuarios/olvide-password", {
+        email,
+      });
       setAlerta({
         msg: data.msg,
-        error: false
-      })
+        error: false,
+      });
     } catch (error) {
       setAlerta({
         msg: error.response.data.msg,
-        error: true
-      })
+        error: true,
+      });
     }
-  }
+  };
   const { msg } = alerta;
   return (
     <>
-      <h1 className="text-sky-600 font-black mb-7 text-6xl capitalize">
-        Recuperar cuenta <span className="text-slate-700">Proyectos</span>
+      <h1 className="text-Secundario_2 font-black mb-7 text-3xl capitalize">
+        Recuperar cuenta
       </h1>
-      <form className="my-7 bg-white shadow rounded-lg px-10 py-5" onSubmit={handleSubmit}>
-      { msg && <Alerta alerta={alerta}/> }
+      <form
+        className="rounded-3xl  p-6 border-Principal_1 border-4"
+        onSubmit={handleSubmit}
+      >
+        {msg && <Alerta alerta={alerta} />}
         <div className="my-1">
           <label
-            className="capitalize text-gray-600 block text-xl font-bold"
+            className="capitalize text-Secundario_2 block text-xl font-bold"
             htmlFor="email"
           >
             Email
@@ -50,30 +53,27 @@ const OlvidePassword = () => {
           <input
             id="email"
             type="email"
-            placeholder="Email de registro"
-            className="w-full my-3 p-3 border rounded-xl bg-gray-50"
+            placeholder="Email De Recuperacion"
+            className="w-full my-7 p-3 border rounded-xl "
             value={email}
-            onChange={ e => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <input
-          type="submit"
-          value="Enviar Instrucciones"
-          className="bg-sky-700 mb-5 w-full py-3 text-white capitalize font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-color"
-        />
+
+        <form className=" flex items-center justify-center">
+          <input
+            type="submit"
+            value="Enviar Instrucciones"
+            className="bg-Principal_3 w-full md:w-4/12 text-black capitalize font-bold  text-center rounded-full border border-Secundario_2 hover:bg-Principal_1 hover:text-white hover:border-Principal_1 hover:border-2 md:mx-1"
+          />
+        </form>
       </form>
-      <nav className="lg:flex lg:justify-between">
+      <nav className="lg:flex p-4 justify-center">
         <Link
-          to="/login-coordinador"
+          to="/LoginAdmin"
           className="block text-center text-slate-500 capitalize text-sm"
         >
           ¿Ya tienes una cuenta? Inicia Sesión
-        </Link>
-        <Link
-          to="/registrar"
-          className="block text-center text-slate-500 capitalize text-sm"
-        >
-          ¿No tienes una cuenta? Regístrate
         </Link>
       </nav>
     </>
