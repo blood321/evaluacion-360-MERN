14530c7fbb55650a745c9fc1bfe4a6b821a26ecf
Eider-Polar <114772645+Eider-Polar@users.noreply.github.com>
2024-03-05 10:13:59 -0500

    index

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
-import conectarDB from './config/db.js';
 import dotenv from "dotenv";
 import cors from 'cors'
-import usuarioRoutes from "./routes/usuario.routes.js";
-import proyectoRoutes from "./routes/proyecto.routes.js";
-import tareaRoutes from "./routes/tarea.routes.js";
+import usuarioRouters from './routes/usuariosRouters.js';
+import fichasRouters from './routes/fichaRouter.js';
+import encuestasRouters from './routes/encuestasRouter.js';
+import  preguntaRouters  from "./routes/preguntaRouter.js";
+import conectarDB from "./config/db.js";
+import  respuestasRouter  from "./routes/respuestasRouter.js";
+import  detalleEncuesta from "./routes/detalleEncuesta.js";
+import  tematicaRouters  from "./routes/tematicaRouters.js";
+import  intructorRouters  from "./routes/intructorRouter.js";
+import programacionRouters from "./routes/programacionRouters.js";
+import aprendzRouter from "./routes/aprendizRouter.js"
+
 
 const app = express();
 app.use(express.json());
@@ -13,10 +21,20 @@ dotenv.config();
 conectarDB();
 app.use(cors());
 
-app.use('/api/usuarios', usuarioRoutes)
-app.use('/api/proyectos', proyectoRoutes)
-app.use('/api/tareas', tareaRoutes)
+app.use('/api/detalleEncuesta',detalleEncuesta)
+app.use('/api/encuesta',encuestasRouters)
+app.use('/api/programacion',programacionRouters)
 
+
+app.use('/api/pregunta',preguntaRouters)
+app.use('/api/tematica',tematicaRouters)
+
+app.use('/api/aprendiz',aprendzRouter)
+app.use('/api/usuarios',usuarioRouters)
+app.use('/api/instructor',intructorRouters)
+app.use('/api/fichas',fichasRouters)
+
+app.use('/api/respuesta',respuestasRouter)
 const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
