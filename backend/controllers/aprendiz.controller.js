1194c20896979c73c5c58a56d0c1281a4a1f4bca
Andres <andresguzman321@gmail.com>
2024-03-05 13:56:32 -0500

    Cambio a las rutas y controladores de aprendiz

--- backend/controllers/aprendiz.controller.js
+++ backend/controllers/aprendiz.controller.js
@@ -1,17 +1,45 @@
-import Aprendiz from "../models/aprendiz";
+import Aprendiz from "../models/aprendiz.js";
+import {
+  emailRegistro,
+  emailAutenticar,
+  emailOlvidePassword,
+} from "../helpers/email.js";
 
 const obtenerAprendiz = async (req, res) => {
-    const data = req.params.email;
-    const email = data;
-    const aprendiz = await Aprendiz.findOne({ email });
-    if (!aprendiz) {
-      const error = new Error("El usuario no existe");
-      return res.status(404).json({ msg: error.message });
-    } else {
-      res.json(aprendiz.nombre);
-    }
-  };
-
-  export {
-    obtenerAprendiz,
+  const data = req.params.email;
+  const email = data;
+  const aprendiz = await Aprendiz.findOne({ email });
+  if (!aprendiz) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  } else {
+    res.json(aprendiz.nombre);
   }
+};
+
+const autenticar = async (req, res) => {
+  const { email } = req.body;
+  const aprendiz = await Aprendiz.findOne({ email });
+  if (!aprendiz) {
+    const error = new Error("El correo no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  try {
+    // usuario.token = generarID();
+    // await usuario.save();
+    // Enviar el email de nueva confirmación
+    emailAutenticar({
+      email: aprendiz.email,
+      nombre: aprendiz.nombre,
+      token: aprendiz.token,
+    });
+    res.json({
+      msg: "Hemos enviado un enlace de acceso a su correo electrónico",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { obtenerAprendiz, autenticar };
--- backend/routes/aprendiz.routes.js
+++ backend/routes/aprendiz.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { obtenerAprendiz } from "../controllers/aprendiz.controller.js";
+import { obtenerAprendiz, autenticar } from "../controllers/aprendiz.controller.js";
 
 const router = express.Router();
 
 router.get("/aviso/:email", obtenerAprendiz);
+router.post("/autenticar", autenticar);
 
 export default router;
