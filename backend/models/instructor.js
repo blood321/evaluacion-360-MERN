d7bec6f09137db06e942ffe05fe01dfa3c4a8c23
Andres <andresguzman321@gmail.com>
2024-03-05 08:50:31 -0500

    cambios para mostrar el nombre del instructor

--- backend/models/instructor.js
+++ backend/models/instructor.js
@@ -1,31 +1,20 @@
 import mongoose from "mongoose";
 
 
-const instructorSchema=mongoose.Schema({
+const instructorSchema = mongoose.Schema({
     nombre:{
         type: String,
         required:true,
         trim:true
     },
   
-    email:{
+    cedula:{
         type: String,
         required:true,
         trim:true,
         unique:true
     },
-    token:{
-        type: String,
-        
-    },
-   
-    equipoEjecutor:     
-    [{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"equipoEjecutor",
-    }]
-
 })
 
-const instructor =mongoose.model('instructor',instructorSchema)
-export default instructor
+const Instructor = mongoose.model('Instructor', instructorSchema)
+export default Instructor
