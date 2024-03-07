9378a1c8754624a434bf460b3ce3c83d37f2ddb0
JUAN CASTILLO <juansebastianq47@gmail.com>
2024-03-07 11:47:24 -0500

    menu hamburguesa

--- frontend/src/components/Sidebar_A.jsx
+++ frontend/src/components/Sidebar_A.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import logoSena from '../assets/img/logoSena.png'
 import {
     FaClipboardList,
@@ -9,12 +8,14 @@ import {
     FaTasks,
     FaWrench,
     FaChevronDown,
+    FaBars,
 } from 'react-icons/fa'
 
 const Sidebar_A = () => {
     const [evaluacionVisible, setEvaluacionVisible] = useState(false)
     const [crearPreguntasVisible, setCrearPreguntasVisible] = useState(false)
     const [flechaAbajo, setFlechaAbajo] = useState(false)
+    const [menuVisible, setMenuVisible] = useState(false) // Nuevo estado para controlar la visibilidad del menú hamburguesa
 
     const toggleEvaluacion = () => {
         setEvaluacionVisible(!evaluacionVisible)
@@ -26,100 +27,109 @@ const Sidebar_A = () => {
         setFlechaAbajo(!flechaAbajo)
     }
 
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible) // Cambia el estado del menú hamburguesa
+    }
+
     return (
         <div className="h-screen px-[25px] overflow-auto bg-white">
+            <FaBars className="md:hidden" onClick={toggleMenu} /> {/* Menú hamburguesa */}
             <div className="px-[15px] py-[10px] flex items-center justify-center border-b-[3px] border-Principal_1/[0.3]">
                 <img src={logoSena} className="w-[50px] h-[50px] md:mt-4 mb-4" alt="Logo sena" />
             </div>
-            <div className="pt-[15px] border-b-[3px] border-Principal_1/[0.2] ">
-                <p className="text-[10px] font-extrabold leading-[16px] text-Secundario_2/[0.5]">Crear en Encuestas</p>
-                <div
-                    className="flex items-center justify-between gap-[10px] py-[15px] cursor-pointer transition-transform hover:translate-x-4 "
-                    onClick={toggleEvaluacion}
-                >
-                    <div className="flex items-center gap-[10px]">
-                        <FaClipboardList color="green" />
-                        <h2 className="font-normal text-[14px] leading-[20px]">Evaluacion-360°</h2>
+            <div className={menuVisible ? 'md:hidden' : 'hidden md:block'}>
+                <div className="pt-[15px] border-b-[3px] border-Principal_1/[0.2] ">
+                    <p className="text-[10px] font-extrabold leading-[16px] text-Secundario_2/[0.5]">
+                        Crear en Encuestas
+                    </p>
+                    <div
+                        className="flex items-center justify-between gap-[10px] py-[15px] cursor-pointer transition-transform hover:translate-x-4 "
+                        onClick={toggleEvaluacion}
+                    >
+                        <div className="flex items-center gap-[10px]">
+                            <FaClipboardList color="green" />
+                            <h2 className="font-normal text-[14px] leading-[20px]">Evaluacion-360°</h2>
+                        </div>
+                        {flechaAbajo ? <FaChevronDown color="green" /> : <FaChevronRight color="green" />}
                     </div>
-                    {flechaAbajo ? <FaChevronDown color="green" /> : <FaChevronRight color="green" />}
-                </div>
-                {evaluacionVisible && (
-                    <ul className="list-disc pl-6 py-[4px] ">
-                        <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  animate-fade-down animate-once animate-duration-[200ms] animate-ease-linear animate-normal  hover:border-b hover:border-Principal_1">
-                            <FaChevronRight color="green" />
-                            <h2 className="font-bold text-sm leading-5 text-Secundario_2">Aprendices</h2>
-                        </li>
-                        <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  animate-fade-down animate-once animate-duration-[250ms] animate-ease-linear animate-normal hover:border-b hover:border-Principal_1">
-                            <FaChevronRight color="green" />
-                            <h2 className="font-bold text-sm leading-5 text-Secundario_2">Compañeros</h2>
-                        </li>
-                        <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  hover:border-b animate-fade-down animate-once animate-duration-[300ms] animate-ease-linear animate-normal hover:border-Principal_1">
-                            <FaChevronRight color="green" />
-                            <h2 className="font-bold text-sm leading-5 text-Secundario_2">Jefes</h2>
-                        </li>
-                        <li className="flex items-center gap-2 pb-2  py-1 cursor-pointer  hover:border-b animate-fade-down animate-once animate-duration-[350ms] animate-ease-linear animate-normal hover:border-Principal_1">
-                            <FaChevronRight color="green" />
-                            <h2 className="font-bold text-sm leading-5 text-Secundario_2">AutoEvaluacion</h2>
-                        </li>
-                    </ul>
-                )}
+                    {evaluacionVisible && (
+                        <ul className="list-disc pl-6 py-[4px] ">
+                            <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  animate-fade-down animate-once animate-duration-[200ms] animate-ease-linear animate-normal  hover:border-b hover:border-Principal_1">
+                                <FaChevronRight color="green" />
+                                <h2 className="font-bold text-sm leading-5 text-Secundario_2">Aprendices</h2>
+                            </li>
+                            <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  animate-fade-down animate-once animate-duration-[250ms] animate-ease-linear animate-normal hover:border-b hover:border-Principal_1">
+                                <FaChevronRight color="green" />
+                                <h2 className="font-bold text-sm leading-5 text-Secundario_2">Compañeros</h2>
+                            </li>
+                            <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  hover:border-b animate-fade-down animate-once animate-duration-[300ms] animate-ease-linear animate-normal hover:border-Principal_1">
+                                <FaChevronRight color="green" />
+                                <h2 className="font-bold text-sm leading-5 text-Secundario_2">Jefes</h2>
+                            </li>
+                            <li className="flex items-center gap-2 pb-2  py-1 cursor-pointer  hover:border-b animate-fade-down animate-once animate-duration-[350ms] animate-ease-linear animate-normal hover:border-Principal_1">
+                                <FaChevronRight color="green" />
+                                <h2 className="font-bold text-sm leading-5 text-Secundario_2">AutoEvaluacion</h2>
+                            </li>
+                        </ul>
+                    )}
 
-                <div
-                    className="flex items-center justify-between gap-[10px] py-[15px] cursor-pointer transition-transform hover:translate-x-6"
-                    onClick={togglePreguntas}
-                >
-                    <div className="flex items-center gap-[10px]">
-                        <FaSpellCheck color="green" />
-                        <h2 className=" font-normal text-[14px] leading-[20px]">Crear Preguntas</h2>
+                    <div
+                        className="flex items-center justify-between gap-[10px] py-[15px] cursor-pointer transition-transform hover:translate-x-6"
+                        onClick={togglePreguntas}
+                    >
+                        <div className="flex items-center gap-[10px]">
+                            <FaSpellCheck color="green" />
+                            <h2 className=" font-normal text-[14px] leading-[20px]">Crear Preguntas</h2>
+                        </div>
+                        {flechaAbajo ? <FaChevronDown color="green" /> : <FaChevronRight color="green" />}
                     </div>
-                    {flechaAbajo ? <FaChevronDown color="green" /> : <FaChevronRight color="green" />}
+                    {crearPreguntasVisible && (
+                        <ul className="list-disc pl-6 py-[4px] ">
+                            <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  animate-fade-down animate-once animate-duration-[200ms] animate-ease-linear animate-normal  hover:border-b hover:border-Principal_1">
+                                <FaChevronRight color="green" />
+                                <h2 className="font-bold text-sm leading-5 text-Secundario_2">Aprendices</h2>
+                            </li>
+                            <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  animate-fade-down animate-once animate-duration-[250ms] animate-ease-linear animate-normal hover:border-b hover:border-Principal_1">
+                                <FaChevronRight color="green" />
+                                <h2 className="font-bold text-sm leading-5 text-Secundario_2">Compañeros</h2>
+                            </li>
+                            <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  hover:border-b animate-fade-down animate-once animate-duration-[300ms] animate-ease-linear animate-normal hover:border-Principal_1">
+                                <FaChevronRight color="green" />
+                                <h2 className="font-bold text-sm leading-5 text-Secundario_2">Jefes</h2>
+                            </li>
+                            <li className="flex items-center gap-2 pb-2  py-1 cursor-pointer  hover:border-b animate-fade-down animate-once animate-duration-[350ms] animate-ease-linear animate-normal hover:border-Principal_1">
+                                <FaChevronRight color="green" />
+                                <h2 className="font-bold text-sm leading-5 text-Secundario_2">AutoEvaluacion</h2>
+                            </li>
+                        </ul>
+                    )}
                 </div>
-                {crearPreguntasVisible && (
-                    <ul className="list-disc pl-6 py-[4px] ">
-                        <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  animate-fade-down animate-once animate-duration-[200ms] animate-ease-linear animate-normal  hover:border-b hover:border-Principal_1">
-                            <FaChevronRight color="green" />
-                            <h2 className="font-bold text-sm leading-5 text-Secundario_2">Aprendices</h2>
-                        </li>
-                        <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  animate-fade-down animate-once animate-duration-[250ms] animate-ease-linear animate-normal hover:border-b hover:border-Principal_1">
-                            <FaChevronRight color="green" />
-                            <h2 className="font-bold text-sm leading-5 text-Secundario_2">Compañeros</h2>
-                        </li>
-                        <li className="flex items-center gap-2 pb-2 py-1 cursor-pointer  hover:border-b animate-fade-down animate-once animate-duration-[300ms] animate-ease-linear animate-normal hover:border-Principal_1">
-                            <FaChevronRight color="green" />
-                            <h2 className="font-bold text-sm leading-5 text-Secundario_2">Jefes</h2>
-                        </li>
-                        <li className="flex items-center gap-2 pb-2  py-1 cursor-pointer  hover:border-b animate-fade-down animate-once animate-duration-[350ms] animate-ease-linear animate-normal hover:border-Principal_1">
-                            <FaChevronRight color="green" />
-                            <h2 className="font-bold text-sm leading-5 text-Secundario_2">AutoEvaluacion</h2>
-                        </li>
-                    </ul>
-                )}
-            </div>
 
-            <div className="pt-[15px] border-b-[3px] border-Principal_1/[0.3] ">
-                <p className="text-[10px] font-extrabold leading-[16px] text-Secundario_2/[0.5]"> Encuestas</p>
-                <div className="flex items-center justify-between gap-[10px] py-[15px] cursor-pointer transition-transform hover:translate-x-4">
-                    <div className="flex items-center gap-[10px] ">
-                        <FaTasks color="green" />
-                        <h2 className=" font-normal text-[14px] leading-[20px]">Encuestas</h2>
+                <div className="pt-[15px] border-b-[3px] border-Principal_1/[0.3] ">
+                    <p className="text-[10px] font-extrabold leading-[16px] text-Secundario_2/[0.5]"> Encuestas</p>
+                    <div className="flex items-center justify-between gap-[10px] py-[15px] cursor-pointer transition-transform hover:translate-x-4">
+                        <div className="flex items-center gap-[10px] ">
+                            <FaTasks color="green" />
+                            <h2 className=" font-normal text-[14px] leading-[20px]">Encuestas</h2>
+                        </div>
+                        <FaChevronRight color="green" />
                     </div>
-                    <FaChevronRight color="green" />
+                    <div className="flex items-center justify-between gap-[10px] py-[15px] cursor-pointer transition-transform hover:translate-x-4">
+                        <div className="flex items-center gap-[10px]">
+                            <FaStream color="green" />
+                            <h2 className=" font-normal text-[14px] leading-[20px]">Resultados</h2>
+                        </div>
+                        <FaChevronRight color="green" />
+                    </div>
                 </div>
-                <div className="flex items-center justify-between gap-[10px] py-[15px] cursor-pointer transition-transform hover:translate-x-4">
-                    <div className="flex items-center gap-[10px]">
-                        <FaStream color="green" />
-                        <h2 className=" font-normal text-[14px] leading-[20px]">Resultados</h2>
+                <div className="pt-[20px]  ">
+                    <div className="flex items-center justify-between gap-[10px] py-[15px] cursor-pointer transition-transform hover:translate-x-4">
+                        <div className="flex items-center gap-[10px] ">
+                            <FaWrench color="green" />
+                            <h2 className=" font-normal text-[14px] leading-[20px]">Configuracion</h2>
+                        </div>
+                        <FaChevronRight color="green" />
                     </div>
-                    <FaChevronRight color="green" />
-                </div>
-            </div>
-            <div className="pt-[20px]  ">
-                <div className="flex items-center justify-between gap-[10px] py-[15px] cursor-pointer transition-transform hover:translate-x-4">
-                    <div className="flex items-center gap-[10px] ">
-                        <FaWrench color="green" />
-                        <h2 className=" font-normal text-[14px] leading-[20px]">Configuracion</h2>
-                    </div>
-                    <FaChevronRight color="green" />
                 </div>
             </div>
         </div>
--- frontend/src/paginas/Admin.jsx
+++ frontend/src/paginas/Admin.jsx
@@ -1,6 +1,7 @@
 import Sidebar_A from '../components/Sidebar_A'
 import Header from '../components/Header'
-import logo2Principal from '../assets/img/logo2Principal.png'
+import Inicio from '../components/inicio'
+import Footer from '../components/Footer'
 
 const Admin = () => {
     document.body.style.overflowY = 'hidden'
@@ -12,27 +13,13 @@ const Admin = () => {
                 </div>
                 <div className="basis-[85%] border">
                     <Header />
-                    <div className="px-auto py-1 flex items-center justify-center ">
-                        <img src={logo2Principal} className="w-[500px] h-[300px] md:mt-1 mb-1" alt="Logo" />
-                    </div>
-                    <div className=" px-[50px] flex items-center justify-center">
-                        <h2 className="text-[28px] font-semibold justify-center text-justify text-Principal_1">
-                            "
-                            <span className="bg-clip-text text-transparent bg-gradient-to-r from-Principal_1 to-Principal_2 font-extrabold text-[30px]">
-                                LAS OPINIONES{' '}
-                            </span>
-                            <span className="bg-clip-text text-transparent bg-gradient-to-r from-Secundario_1 to-Secundario_2 ">
-                                de los Aprendices guían y enriquecen a los Instructores
-                            </span>
-                            , Mejorando la Calidad de la Educación"
-                        </h2>
+                    <div>
+                        <Inicio />
                     </div>
                 </div>
             </div>
             <div>
-                <footer className="text-center  text-Principal_1 p-3 w-full mt-5 md:bg-Principal_1 md:text-Principal_3 md:fixed bottom-0 inset-x-0">
-                    &copy; 2024 SENA Centro Agropecuario
-                </footer>
+                <Footer />
             </div>
         </>
     )
