e607d25d1979e6dce57dba7b075623efead8f63e
sebas313313313 <potosliping8@gmail.com>
2024-03-08 08:37:48 -0500

    guardado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,6 +45,7 @@ function App() {
             </Route>
             <Route path="admin" element={<Admin />} />
 
+            <Route path="/aviso" element={<Aviso />} />
             <Route path="/Login" element={<LoginAdmin />} />
             <Route path="InicioAdmin" element={<IniAdmin />} />
             <Route path="login-adm" element={<LoginAdmin />} />
@@ -62,6 +63,8 @@ function App() {
               <Route path="Listar-encuestas" element={<ListarEncuestas />} />
               <Route path="Resultados" element={<Resultados />} />
             </Route>
+
+
 
             <Route path="proyectos" element={<RutaProtegida />}>
               <Route index element={<Proyectos />} />
