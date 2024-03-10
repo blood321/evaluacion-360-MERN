8146a427601fd9c65b0332992e75ed7e1f2ddedb
Eider SAmyr Daza Muñoz <esamyrd@gmail.com>
2024-03-10 11:51:14 -0500

    tematicaROuters

--- backend/routes/tematicaRouters.js
+++ backend/routes/tematicaRouters.js
@@ -2,15 +2,7 @@ import Express  from "express";
 import {
  nuevaTematica
 } from '../controllers/tematicaController.js'
-
-
 const router = Express.Router()
 router.route('/')
-    
     .post(nuevaTematica)
-
-
-    
-
-
 export default router
