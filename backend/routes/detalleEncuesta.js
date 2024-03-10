a19bebcd37a5f711e2a69c9808d9fe3ac57c0613
Eider SAmyr Daza Muñoz <esamyrd@gmail.com>
2024-03-10 11:42:20 -0500

    inplementacion del backend

--- backend/routes/detalleEncuesta.js
+++ backend/routes/detalleEncuesta.js
@@ -1,6 +1,6 @@
 import Express  from "express";
 import {
- nuevoDetalleEncuesta,obtenerDetallesEcuestas,mostrasRespuestas
+ nuevoDetalleEncuesta,obtenerDetallesEcuestas
 } from '../controllers/detalleEncuestaController.js'
 import checkAuth from '../middleware/checkAuth.js'
 
@@ -8,10 +8,9 @@ const router = Express.Router()
 router.route('/')
     
     .post(nuevoDetalleEncuesta)
-    router.route('/responde/:email')
-    .get(obtenerDetallesEcuestas)
-    router.route('/responde-encuesta')
-    .get(mostrasRespuestas)
+    router.route('/responde2')
+    
+    .get(checkAuth,obtenerDetallesEcuestas)
     
 
 
--- backend/routes/tematicaRouters.js
+++ backend/routes/tematicaRouters.js
@@ -2,7 +2,15 @@ import Express  from "express";
 import {
  nuevaTematica
 } from '../controllers/tematicaController.js'
+
+
 const router = Express.Router()
 router.route('/')
+    
     .post(nuevaTematica)
+
+
+    
+
+
 export default router
