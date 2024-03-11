b98680d9150256c10b3394b8ba508bfc67791785
sebas313313313 <potosliping8@gmail.com>
2024-03-11 09:14:56 -0500

    se implemento los componente en las paginas

--- frontend/src/paginas/Crear-encuestas-aprendices.jsx
+++ frontend/src/paginas/Crear-encuestas-aprendices.jsx
@@ -1,7 +1,18 @@
+import Tematicas from "../components/Tematicas";
+import FormularioProyecto from "../components/FormularioProyecto";
 
 const CrearEncuestasAprendices = () => {
     return (
       <>
+      <div className="flex flex-row items-center">
+      <Tematicas></Tematicas>
+      <FormularioProyecto></FormularioProyecto>
+      </div>
+      <div>
+      </div>
+      <div className="">
+
+      </div>
       estamos en la creacion de la encuesta para aprendices 
       </>
     );
