5317b7eaecfd3fc3f56fc06189b06f38981091a0
Andres <andresguzman321@gmail.com>
2024-04-10 16:26:00 -0500

    tematicas update

--- backend/routes/tematicaRouters.js
+++ backend/routes/tematicaRouters.js
@@ -1,10 +1,8 @@
 import Express  from "express";
 import {
- nuevaTematica,tematicas
+ nuevaTematica
 } from '../controllers/tematicaController.js'
 const router = Express.Router()
 router.route('/')
     .post(nuevaTematica)
- router.route("/listar-tematicas")
-    .get(tematicas)
 export default router
