5304574034c6bd8aa8b934cb856d2b032f4f1305
sebas313313313 <potosliping8@gmail.com>
2024-04-17 10:53:05 -0500

    prueva

--- frontend/src/components/EditarEncuesta.jsx
+++ frontend/src/components/EditarEncuesta.jsx
@@ -193,7 +193,7 @@ function Editar({ onClose }) {
         onClick={handleSave}
         className="mb-2 md:mb-0 bg-green-400 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-green-500"
       >
-        Guardar
+        Guardarr
       </button>
     </div>
                   </div>
