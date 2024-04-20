42081080393d9660b38834ce88f6b3ad9e3301b0
sebas313313313 <potosliping8@gmail.com>
2024-04-20 18:36:15 -0500

    funcion de subir archivos medianamente terminada

--- frontend/src/paginas/Excel.jsx
+++ frontend/src/paginas/Excel.jsx
@@ -1,50 +1,111 @@
-import { FaUsers } from "react-icons/fa";
-import { BsFiletypeXlsx } from "react-icons/bs";
-import { useState,useEffect } from "react";
+import { BsClipboard2Check } from "react-icons/bs";
+import { FaUserGraduate } from "react-icons/fa6";
+import { FaUsersRectangle } from "react-icons/fa6";
+import { FaUserPen } from "react-icons/fa6";
+import { useState, useEffect } from "react";
 import clienteAxios from "../config/clienteAxios";
+
 const excel = () => {
-  const [documento, setDocumento] = useState();
+  const [documento, setDocumento] = useState(null);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Mostrar el modal al enviar el formulario
- 
-  };
-  useEffect(() => {
-
-
-    async function loadTematicas() {
+    handleFileChange(e); // Add this line
+    if (documento) {
       try {
-        const response = await clienteAxios.post('/Excel/fichas',documento);
-        console.log(response)
+        const response = await clienteAxios.post("/Excel/fichas", documento);
+        console.log(response);
       } catch (error) {
-        console.error('Error fetching tematicas:', error);
+        console.error("Error fetching tematicas:", error);
       }
     }
-    loadTematicas();
-  }, []);
-  console.log(documento);
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (
+      file &&
+      file.type ===
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    ) {
+      setDocumento(file);
+    } else {
+      alert("Solo se permiten archivos Excel (.xlsx)");
+    }
+  };
 
   return (
     <div className="py-4">
-      <div className="  bg-gradient-to-r from-Principal_1 to-Principal_2 text-white font-bold py-2 px-4 rounded">
-        <FaUsers />
+      <div className=" my-1 bg-gradient-to-r from-Principal_1 to-Principal_2 text-white font-bold py-2 px-4 rounded">
+        <FaUserGraduate />
         subir Aprendices
+        <div className="flex">
+          <input
+            type="file"
+            className="flex-grow"
+            onChange={handleFileChange}
+          />
+          <button
+            className="bg-Principal_1 text-white px-4 py-2 rounded"
+            onClick={handleSubmit}
+            disabled={!documento}
+          >
+            Subir</button>
+        </div>
       </div>
-      <div className=" py-4 my-1 bg-gradient-to-r from-Principal_1 to-Principal_2 text-white font-bold py-2 px-4 rounded">
-        <BsFiletypeXlsx />
+      <div className=" my-1 bg-gradient-to-r from-Principal_1 to-Principal_2 text-white font-bold py-2 px-4 rounded">
+        <BsClipboard2Check />
         Subir programacion
+        <div className="flex">
+          <input
+            type="file"
+            className="flex-grow"
+            onChange={handleFileChange}
+          />
+          <button
+            className="bg-Principal_1 text-white px-4 py-2 rounded"
+            onClick={handleSubmit}
+            disabled={!documento}
+          >
+            Subir
+          </button>
+        </div>
       </div>
-      <div className=" py-4 my-1 bg-gradient-to-r from-Principal_1 to-Principal_2 text-white font-bold py-2 px-4 rounded">
-        <BsFiletypeXlsx />
-        subir Instructores
+      <div className=" my-1 bg-gradient-to-r from-Principal_1 to-Principal_2 text-white font-bold py-2 px-4 rounded">
+        <FaUsersRectangle />
+        Subir instructores
+        <div className="flex">
+          <input
+            type="file"
+            className="flex-grow"
+            onChange={handleFileChange}
+          />
+          <button
+            className="bg-Principal_1 text-white px-4 py-2 rounded"
+            onClick={handleSubmit}
+            disabled={!documento}
+          >
+            Subir
+          </button>
+        </div>
       </div>
-      <div className=" py-4 my-1 bg-gradient-to-r from-Principal_1 to-Principal_2 text-white font-bold py-2 px-4 rounded">
-        <BsFiletypeXlsx />
-        <input
-          type="file"
-          onChange={(e) => setDocumento(e.target.value)}
-        ></input>
+      <div className=" my-1 bg-gradient-to-r from-Principal_1 to-Principal_2 text-white font-bold py-2 px-4 rounded">
+        <FaUserPen />
         subir fichas
+        <div className="flex">
+          <input
+            type="file"
+            className="flex-grow"
+            onChange={handleFileChange}
+          />
+          <button
+            className="bg-Principal_1 text-white px-4 py-2 rounded"
+            onClick={handleFileChange}
+            disabled={!documento}
+          >
+            Subir
+          </button>
+        </div>
       </div>
     </div>
   );
