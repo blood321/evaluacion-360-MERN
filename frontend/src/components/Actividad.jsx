ea157e09c76bfe95e0199f08e3aad705eafdc638
sebas313313313 <potosliping8@gmail.com>
2024-04-25 15:45:43 -0500

    componente areglado.

--- frontend/src/components/Actividad.jsx
+++ frontend/src/components/Actividad.jsx
@@ -21,69 +21,93 @@ function Actividad({ onClose }) {
         zIndex: "9999",
       }}
     >
-      <section className="h-screen px-4 text-gray-600 antialiased">
-        <div className="flex h-full flex-col justify-center">
-          <div className="mx-auto w-full max-w-2xl rounded-sm border border-gray-800 bg-white shadow-lg">
-            <header className="border-b border-gray-100 px-5 py-4">
-              <div className="font-semibold text-gray-800">
-                Datos de encuesta
-              </div>
-            </header>
+        <div className="mx-auto w-full max-w-2xl rounded-xl border bg-white shadow-lg">
+          <header className="border-b border-gray-100 px-5 py-2">
+            <div className="font-semibold text-gray-800">Datos de la encuesta</div>
+          </header>
 
-            <div className="overflow-x-auto p-3">
-              <table className="w-full table-auto">
-                <thead className="bg-gray-50 text-xs font-semibold uppercase text-gray-400">
-                  <tr>
-                    <th className="p-2">
-                      <div className="text-left font-semibold">
-                        fecha de lanzamiento
-                      </div>
-                    </th>
+          <div className="overflow-x-auto p-3">
+            <table className="w-full table-auto">
+              <thead className="bg-gray-50 text-xs font-semibold uppercase text-gray-500">
+                <tr>
+                  <th className="p-2">
+                    <div className="text-left font-semibold">
+                      fecha de lanzamiento
+                    </div>
+                  </th>
 
-                    <th className="p-2">
-                      <div className="text-left font-semibold">
-                        Cantidad de respuestas
-                      </div>
-                    </th>
-                  </tr>
-                </thead>
+                  <th className="p-2">
+                    <div className="text-left font-semibold">
+                      Cantidad de respuestas
+                    </div>
+                  </th>
+                </tr>
+              </thead>
 
-                <tbody className="divide-y divide-gray-100 text-sm">
-                  <tr>
-                    <td className="p-2">
-                      <div className="font-medium text-gray-800">fecha uno</div>
-                    </td>
-                    <td className="p-2">
-                      <div className="text-left font-medium text-green-500">
-                        5 respuestas
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="p-2">
-                      <div className="font-medium text-gray-800">fecha uno</div>
-                    </td>
-                    <td className="p-2">
-                      <div className="text-left font-medium text-green-500">
-                        5 respuestas
-                      </div>
-                    </td>
-                  </tr>
-                  <div className="mt-5 text-right md:space-x-3">
-                    <button
-                      className="mb-2 flex md:mb-0 bg-gray-300 px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-black rounded-full hover:shadow-lg hover:bg-gray-400"
-                      onClick={onClose}
-                    >
-                      Cerrar
-                    </button>
-                  </div>
-                </tbody>
-              </table>
-            </div>
+              <tbody className="divide-y divide-gray-100 text-sm">
+                <tr>
+                  <td className="p-2">
+                    <div className="font-medium text-gray-800">fecha uno</div>
+                  </td>
+                  <td className="p-2">
+                    <div className="text-left font-medium text-green-500">
+                      5 respuestas
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2">
+                    <div className="font-medium text-gray-800">fecha uno</div>
+                  </td>
+                  <td className="p-2">
+                    <div className="text-left font-medium text-green-500">
+                      5 respuestas
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2">
+                    <div className="font-medium text-gray-800">fecha uno</div>
+                  </td>
+                  <td className="p-2">
+                    <div className="text-left font-medium text-green-500">
+                      5 respuestas
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2">
+                    <div className="font-medium text-gray-800">fecha uno</div>
+                  </td>
+                  <td className="p-2">
+                    <div className="text-left font-medium text-green-500">
+                      5 respuestas
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2">
+                    <div className="font-medium text-gray-800">fecha uno</div>
+                  </td>
+                  <td className="p-2">
+                    <div className="text-left font-medium text-green-500">
+                      5 respuestas
+                    </div>
+                  </td>
+                </tr>
+                <div className="mt-5 text-right md:space-x-3">
+                  <button
+                    className="mb-2 flex md:mb-0 bg-gray-300 px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-black rounded-full hover:shadow-lg hover:bg-gray-400"
+                    onClick={onClose}
+                  >
+                    Cerrar
+                  </button>
+                </div>
+              </tbody>
+            </table>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
   );
 }
 
