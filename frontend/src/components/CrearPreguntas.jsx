05d22dedec792c7e0817b2d665024b5d486a8f18
sebas313313313 <potosliping8@gmail.com>
2024-05-08 14:43:13 -0500

    cambio de rutas inservibles en app.jsx

--- frontend/src/components/CrearPreguntas.jsx
+++ frontend/src/components/CrearPreguntas.jsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from "react";
-import usePreguntas from "../hooks/usePreguntas";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import usePreguntas from "../hooks/usePreguntas";
 
 const CrearPreguntas = ({ tematica }) => {
   const [pregunta, setPregunta] = useState("");
   const [mostrarAlerta, setMostrarAlerta] = useState(false);
   const { submitPregunta } = usePreguntas();
-  const [encuestado, setEncuestado] = useState(""); // Estado para guardar el valor de 'encuestado'
+  const [encuestado, setEncuestado] = useState(""); 
 
-  // Utiliza useLocation para obtener la ruta actual
   const location = useLocation();
 
-  // Establece 'encuestado' basado en la ruta actual
   useEffect(() => {
     if (location.pathname === "/inicio-admin/Crear-preguntas-aprendices") {
       setEncuestado("Aprendiz");
@@ -23,92 +21,39 @@ const CrearPreguntas = ({ tematica }) => {
       setEncuestado("Jefes");
     }
   }, [location]);
-<<<<<<< HEAD
-    console.log(pregunta)
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-        if ([pregunta].includes("")) {
-            setMostrarAlerta({
-            msg: "Todos los campos son necesarios ",
-            error: true,
-          });
-          return;
-        }
-        // Pasar los datos hacia el provider
-        await submitPregunta({ pregunta, tematica,encuestado});
-    
-        setPregunta("");
-      };
-    
-    return (
-        <form               className="bg-white/[0.2] rounded-md  px-8 w-[400px] md:w-[480px]"
-        onSubmit={handleSubmit}
-        >
 
-        <div className="px-auto p-3">
-            <h2 className="font-extrabold text-[30px] text-Principal_2/[0.9] mb-2">Crear Preguntas</h2>
-            <div className="flex items-center">
-                <input
-                    type="text"
-                    placeholder="Escribe tu pregunta aquí"
-                    className="border-2 border-gray-300 p-2 mr-2 rounded-md w-[500px]"
-                    value={pregunta}
-                    onChange={e => setPregunta(e.target.value)}
-                />
-                <button
-                    className="bg-gradient-to-r from-Principal_1 to-Principal_2 text-white font-bold py-2 px-4 rounded"
-                >
-                    Crear
-                </button>
-            </div>
-            {mostrarAlerta && (
-                <p className="text-red-500 mt-2 font-extrabold">Por favor, escribe una pregunta antes de crearla.</p>
-            )}
-=======
-  console.log(pregunta);
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if ([pregunta].includes("")) {
-      setMostrarAlerta({
-        msg: "Todos los campos son necesarios ",
-        error: true,
-      });
+    if (!pregunta.trim()) {
+      setMostrarAlerta(true);
       return;
     }
-    // Pasar los datos hacia el provider
+
     await submitPregunta({ pregunta, tematica, encuestado });
 
     setPregunta("");
+    setMostrarAlerta(false);
   };
 
   return (
-    <form
-      className="bg-white/[0.2] rounded-md px-10 w-[400px] md:w-[480px]"
-      onSubmit={handleSubmit}
-    >
-      <div className="px-auto">
-        <h2 className="font-extrabold text-[25px] text-Principal_2/[0.9]">
-          Crear Preguntas
-        </h2>
-        <div className="flex p-3 items-center">
+    <form className="bg-white bg-opacity-20 rounded-md px-8 w-[400px] md:w-[480px]" onSubmit={handleSubmit}>
+      <div className="p-3">
+        <h2 className="font-extrabold text-[30px] text-Principal_2 mb-2">Crear Preguntas</h2>
+        <div className="flex items-center">
           <input
             type="text"
             placeholder="Escribe tu pregunta aquí"
-            className="border-2 border-gray-300 p-2 mr-2 rounded-md w-[500px]"
+            className="border-2 border-gray-300 p-2 mr-2 rounded-md w-[300px]"
             value={pregunta}
             onChange={(e) => setPregunta(e.target.value)}
           />
-          <button class="middle none center mr-3 rounded-lg bg-gradient-to-tr from-Principal_1 to-Principal_2 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-Principal_1/20 transition-all hover:shadow-lg hover:shadow-Principal_1/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+          <button className="bg-gradient-to-r from-Principal_1 to-Principal_2 text-white font-bold py-2 px-4 rounded">
             Crear
           </button>
->>>>>>> e49351a5461b00196ccb9431ddde24da3f2e16d7
         </div>
         {mostrarAlerta && (
-          <p className="text-red-500 mt-2 font-extrabold">
-            Por favor, escribe una pregunta antes de crearla.
-          </p>
+          <p className="text-red-500 mt-2 font-extrabold">Por favor, escribe una pregunta antes de crearla.</p>
         )}
       </div>
     </form>
--- frontend/src/components/EditarEncuesta.jsx
+++ frontend/src/components/EditarEncuesta.jsx
@@ -94,123 +94,34 @@ function Editar({ onClose, kuko }) {
   }, [tematicaSeleccionada, preguntas]);
 
   return (
-      <>
-          {showModal && (
-              <div className="fixed inset-0 flex items-center justify-center z-50">
-                  <div className="absolute inset-0 bg-black opacity-60"></div>
-                  <div className="max-w-md w-full space-y-8 p-10 bg-white rounded-xl shadow-lg z-50">
-                      <div className="grid gap-8 grid-cols-1">
-                          <div className="flex flex-col">
-                              <div className="flex flex-col sm:flex-row items-center">
-                                  <h2 className="font-semibold text-lg mr-auto">
-                                      <span className="bg-clip-text text-transparent bg-gradient-to-r from-Principal_1 to-Principal_2">
-                                          Editar Encuesta
-                                      </span>
-                                  </h2>
-                                  <div className="w-full sm:w-auto sm:ml-auto mt-3 sm:mt-0"></div>
-                              </div>
+    <>
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-black opacity-60"></div>
+          <div className="max-w-md w-full space-y-8 p-10 bg-white rounded-xl shadow-lg z-50">
+            <div className="grid gap-8 grid-cols-1">
+              <div className="flex flex-col">
+                <div className="flex flex-col sm:flex-row items-center">
+                  <h2 className="font-semibold text-lg mr-auto">
+                    Editar Encuesta
+                  </h2>
+                  <div className="w-full sm:w-auto sm:ml-auto mt-3 sm:mt-0"></div>
+                </div>
 
-                              <div className="mt-5">
-                                  <div className="form">
-                                      <div className="md:flex flex-row md:space-x-4 w-full text-xs">
-                                          <div className="mb-3 space-y-2 w-full text-xs">
-                                              <label className="font-semibold text-gray-600 py-2">
-                                                  Renombrar Encuesta
-                                              </label>
-                                              <input
-                                                  placeholder="nombre actual"
-                                                  className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4"
-                                                  required="required"
-                                                  type="text"
-                                              />
-                                          </div>
-                                          <div className="mb-3 space-y-2 w-full text-xs">
-                                              <label className="font-semibold text-gray-600 py-2">Temáticas
-                                              </label>
-                                              <select
-                                                  className="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 md:w-full "
-                                                  required="required"
-                                                  value={tematicaSeleccionada}
-                                                  onChange={handleTematicaChange}
-                                              >
-                                                  <option value="">Selecciona Temática</option>
-                                                  {/* Mapear las temáticas disponibles */}
-                                                  {tematicas.map(tematica => (
-                                                      <option key={tematica._id} value={tematica._id}>
-                                                          {tematica.tematica}
-                                                      </option>
-                                                  ))}
-                                              </select>
-                                              <p className="text-sm text-red-500 hidden mt-3" id="error">
-                                                  Please fill out this field.
-                                              </p>
-                                          </div>
-                                      </div>
-
-                                      <div className="mb-3 space-y-2 w-full text-xs">
-                                          <label className="font-semibold text-gray-600 py-2">Preguntas</label>
-                                          <select
-                                              className="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 md:w-full "
-                                              required="required"
-                                              multiple
-                                              value={preguntasSeleccionadas}
-                                              onChange={handlePreguntasChange}
-                                          >
-                                              <option value="">Selecciona Pregunta</option>
-                                              {/* Mapear las preguntas filtradas según la temática seleccionada */}
-                                              {preguntasFiltradas.map(pregunta => (
-                                                  <option key={pregunta._id} value={pregunta._id}>
-                                                      {pregunta.pregunta}
-                                                  </option>
-                                              ))}
-                                          </select>
-                                          <p className="text-sm text-red-500 hidden mt-3" id="error">
-                                              Please fill out this field.
-                                          </p>
-                                      </div>
-
-                                      <div className="flex-auto w-full mb-1 text-xs space-y-2">
-                                          <label className="font-semibold text-gray-600 py-2">Descripción</label>
-                                          <textarea
-                                              required=""
-                                              name="message"
-                                              id=""
-                                              className="min-h-[100px] max-h-[300px] h-20 appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg  py-4 px-4"
-                                              placeholder="Descripcion Actual"
-                                              spellCheck="false"
-                                          ></textarea>
-                                      </div>
-                                      <div className="mb-3 space-y-2 w-full text-xs">
-                                          <label className="font-semibold text-gray-600 py-2">Fecha Limite</label>
-                                          <input
-                                              placeholder="fecha actual"
-                                              className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4"
-                                              required="required"
-                                              type="datetime-local"
-                                          />
-                                      </div>
-
-                                      <div className="mt-5 text-right md:space-x-3 md:block flex flex-col-reverse">
-                                          <button
-                                              onClick={handleCancel}
-                                              className="mb-2 md:mb-0 bg-red-500  border-red-500  hover:bg-red-600 text-white  px-5 py-2 text-sm shadow-sm font-medium tracking-wider border  rounded-full hover:shadow-lg "
-                                          >
-                                              Cancelar
-                                          </button>
-                                          <button
-                                              onClick={handleSave}
-                                              className="mb-2 md:mb-0 bg-gradient-to-r from-Principal_1 to-Principal_2 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-green-500"
-                                          >
-                                              Guardar
-                                          </button>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
+                <div className="mt-5">
+                  <div className="form">
+                    <div className="md:flex flex-row md:space-x-4 w-full text-xs">
+                      <div className="mb-3 space-y-2 w-full text-xs">
+                        <label className="font-semibold text-gray-600 py-2">
+                          Renombrar Encuesta
+                        </label>
+                        <input
+                          placeholder="nombre actual"
+                          className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4"
+                          required="required"
+                          type="text"
+                        />
                       </div>
-<<<<<<< HEAD
-                  </div>
-=======
                       <div className="mb-3 space-y-2 w-full text-xs">
                         <label className="font-semibold text-gray-600 py-2">
                           Temáticas
@@ -289,11 +200,13 @@ function Editar({ onClose, kuko }) {
                     </div>
                   </div>
                 </div>
->>>>>>> e49351a5461b00196ccb9431ddde24da3f2e16d7
               </div>
-          )}
-      </>
-  )
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Editar;
